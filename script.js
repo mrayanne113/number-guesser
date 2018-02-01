ecaff84a6b5a40763dfcdc980ea347c7cfb801cf
Megan Kaiser <meganrayanne@yahoo.com>
2018-02-01 11:31:21 -0700

    Final push before evals

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ var guessLevel = document.querySelector('guess-level');
 var count = 0;
 var guessCount = 0;
 var randomNumber = 0;
+var userMax;
+var userMin = document.querySelector('.new-min').value;
 
 
 // event listeners
@@ -25,12 +27,12 @@ levelUpBttn.addEventListener('click', userRandomNumber);
 // on page load
 disableBttns();
 newNumber();
-toggleClass();
+
 
 // functions
 function newNumber(max, min) {
-  var max = 100;
-  var min = 1;
+  max = 100;
+  min = 1;
   randomNumber = parseInt(Math.round(Math.random() * (max - min) + min)); 
   console.log('Cheater the answer is ' + randomNumber)
   return randomNumber;
@@ -46,11 +48,10 @@ function disableBttns() {
 function enableBttns() {
   clearField.disabled = false;
   guessBttn.disabled = false;
-  resetGame.disabled = false;
+  resetGame.disabled = false; 
 }
 
 function theUsersGuess() {
-  console.log('users/guessCount')
   event.preventDefault();
   var guess = userGuess.value;
   var guessParse = parseInt(guess);
@@ -64,15 +65,13 @@ function clearInput() {
   event.preventDefault();
   userGuess.value = '';
   clearField.disabled = true;
-  // userMinInput.value = '';
-  // userMaxInput.value = '';
 }
 
-function feedbackOfGuess(guessParse) {
-  console.log('feedback/Of/Guess')
+function feedbackOfGuess(guessParse, userMin, userMax) {
+  console.log(userMax, userMin)
   if (randomNumber === guessParse) {
     checkLevel();
-  } else if (guessParse > 100 || guessParse < 1){
+  } else if (guessParse > 100 || guessParse < 1 ){
     showUserGuess.innerText = 'That\'s not in the range, silly';
     lowOrHigh.innerText = 'Error';
   } else if (randomNumber > guessParse) {
@@ -91,14 +90,14 @@ function feedbackOfGuess(guessParse) {
 function checkLevel() {
   levelUp();
   levelTwo();
-
   if (count >= 2){
   openByTen();  
   }
 }
 
+
 function resetTheGame() {
-  clearInput();
+  disableBttns();
   newNumber();
   count = 0;
   guessCount = 0;
@@ -109,6 +108,7 @@ function resetTheGame() {
   guessBttn.classList.remove('top-bttn');
   clearField.classList.remove('top-bttn');
   resetGame.classList.remove('reset-blue');
+  levelUpBttn.classList.add('display-none');
   showUserGuess.style.color = '#EB008B';
   showUserGuess.style.color = '#EB008B';
   showUserGuess.innerText = '??';
@@ -120,6 +120,7 @@ function userRandomNumber() {
   userGuess.disabled = false;
   max = document.querySelector('.new-max').value;
   min = document.querySelector('.new-min').value;
+  document.querySelector('.new-range').innerText = 'Range ' + min + ' - ' + max;
   var parseMax = parseInt(max);
   var parseMin = parseInt(min);
   randomNumber = Math.floor(Math.random() * (parseMax - parseMin) + parseMin); 
@@ -128,7 +129,6 @@ function userRandomNumber() {
 }
 
 function levelUp() {
-  console.log('level/up')
   document.querySelector('.header-h1').style.color = '#71A2B6';
   userGuess.style.color = '#71A2B6';
   guessBttn.classList.remove('guess-number');
@@ -137,7 +137,6 @@ function levelUp() {
 }
 
 function levelTwo() {
-  console.log('level/two')
   count++;
   guessCount++;
   showUserGuess.style.color = '#71A2B6';
@@ -153,19 +152,17 @@ function levelTwo() {
 }
 
 function openByTen() {
+  userGuess.disabled = false;
   max = userMaxInput.value;
   min = userMinInput.value;
   var parseMax = parseInt(max) + 10;
   var parseMin = parseInt(min) - 10;
+  document.querySelector('.new-range').innerText = 'Range ' + parseMin + ' - ' + parseMax;
   randomNumber = Math.floor(Math.random() * (parseMax - parseMin) + parseMin); 
   userMinInput.value = parseMin;
   userMaxInput.value = parseMax;
   console.log('random ' + randomNumber)
   showUserGuess.innerText = 'now +10 -10';
   return randomNumber;
-}
-
-function toggleClass() {
-   // guessBttn.classList.toggle('guess-level');
 }
 
